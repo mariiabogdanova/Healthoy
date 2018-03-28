041a6428076b7507b9409adfe4b5505107fc76f3
mariiabogdanova <februarymary@gmail.com>
2018-03-28 11:59:37 +0300

    database connection php 1

--- APP/HealthOyProject/connection/get_video.php
+++ APP/HealthOyProject/connection/get_video.php
@@ -1,13 +1,13 @@
 <?php
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$db_name = 'healthdev';
+// $host = 'localhost';
+// $username = 'root';
+// $password = '';
+// $db_name = 'healthdev';
 
-// $host = 'mysli.oamk.fi';
-// $username = 't6ishe01';
-// $password = 'WA7oRTrHLUB5qZUa';
-// $db_name = 'opisk_t6ishe01';
+$host = 'mysli.oamk.fi';
+$username = 't6ishe01';
+$password = 'WA7oRTrHLUB5qZUa';
+$db_name = 'opisk_t6ishe01';
 
 $mysql = @mysqli_connect($host, $username, $password, $db_name );
 
