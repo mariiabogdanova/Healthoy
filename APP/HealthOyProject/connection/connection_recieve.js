302a0e2122ea7c2dee020bb2cf89500c02101341
mariiabogdanova <februarymary@gmail.com>
2018-04-09 10:29:52 +0300

    connection php header

--- APP/HealthOyProject/connection/connection_recieve.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-$.post( "http://www.students.oamk.fi/~t6ishe01/Healthdev/get_video.php", function( data ) {
-  // alert( "Data Loaded: " + data );
-  console.log("Data loaded" + data);
-});
-
-});
-
