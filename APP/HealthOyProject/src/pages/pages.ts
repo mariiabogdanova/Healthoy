f7dc6ed77066d958d19f453c26cf4e6d3f475aba
mariiabogdanova <februarymary@gmail.com>
2018-04-18 12:12:11 +0300

    Settings page
    Almost complete

--- APP/HealthOyProject/src/pages/pages.ts
+++ APP/HealthOyProject/src/pages/pages.ts
@@ -5,10 +5,13 @@ export const FirstRunPage = 'TabsPage';//TutorialPage
 // Change this if not using tabs
 export const MainPage = 'TabsPage';
 export const confetti_modal= 'ConfettiModalPage';
+export const Settings = 'SettingsPage';
 
 
 // The initial root pages for our tabs (remove if not using tabs)
 export const Tab1Root = 'WorkoutPage';
 export const Tab2Root = 'SchedulepagePage';
 export const Tab3Root = 'ProfilePage';
+
+
 // export const Tab3Root = 'SettingsPage';
--- APP/HealthOyProject/src/pages/profile/profile.ts
+++ APP/HealthOyProject/src/pages/profile/profile.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SettingsPage } from '../settings/settings';
+
 
 /**
  * Generated class for the ProfilePage page.
@@ -16,10 +18,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class ProfilePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  	
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
   }
+
+
+openSettings() {
+    this.navCtrl.push('SettingsPage');
+  }
+
 
 }
